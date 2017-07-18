d85859cdaf04a67c10f9b684054672d83b0fb4b9
Jesper Blad Jensen <deldy@deldysoft.dk>
2017-07-18 11:21:10 +0200

    Boing Boing!

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import * as PIXI from "pixi.js";
 import keyboard from "./keyboard";
+import { Howl } from "howler";
 
 var type = "WebGL";
 if (!PIXI.utils.isWebGLSupported()) {
   type = "canvas";
 }
+
+var sound = new Howl({
+  src: ["sounds/boing.mp3"]
+});
 
 PIXI.utils.sayHello(type);
 
@@ -63,21 +68,30 @@ function setup() {
 
     if (sprite.x < 0) {
       sprite.x = 0;
+      boing();
     }
     if (sprite.y < 0) {
       sprite.y = 0;
+      boing();
     }
     if (sprite.y + sprite.height > 256) {
       sprite.y = 256 - sprite.height;
+      boing();
     }
     if (sprite.x + sprite.width > 256) {
       sprite.x = 256 - sprite.width;
+      boing();
     }
 
     //Render the stage to see the animation
     renderer.render(stage);
   }
 
+  function boing() {
+    if (!sound.playing()) {
+      sound.play();
+    }
+  }
   //Start the game loop
   gameLoop();
 }
